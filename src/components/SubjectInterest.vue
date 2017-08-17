68b61fac149856a85d06780aea9c5daaafb2b204
Michael Lee <mtlee@michaels-mbp.fhcrc.org>
2017-08-17 09:10:56 -0700

    added logic for buttons to toggle color

--- src/components/SubjectInterest.vue
+++ src/components/SubjectInterest.vue
@@ -1,16 +1,15 @@
 <template>
   <v-app>
     <div class="row progressBar">
-        <div class="indicatorEmpty offset"> </div>
+        <div class="indicatorEmpty offset" > </div>
         <div class="indicatorEmpty offset one"> </div>
         <div class="indicatorEmpty offset two"> </div>
         <div class="indicatorEmpty offset three"> </div>
         <div class="indicatorEmpty offset four"> </div>
         <div class="indicatorEmpty offset five"> </div>
-    </div>
+    </div>  
     <br>
     <br>
-  
     <div class="row">
       <p class="col-sm-8 ml-6 "> Why are you interested in joining mPower? </p>
       <p class="col-md-10 ml-6 text-left tiny lead"> Select your reasons by choosing below. You can remove them by clicking the X. </p>
@@ -18,104 +17,105 @@
       <br>
   
       <p class="lead col-sm-8 ml-6"> I'm interested in the joining mPower because I </p>
-      <v-btn v-model="selectedChoices" class="ml-6 round lower long lightPurple" @click="handleClick(0)" v-bind:class="{ 'lightPurple': isLightPurple(0), 'darkPurple': isDarkPurple(0)}"> Want to help myself
+      <v-btn v-model="selectedChoice[0].color" class="ml-6 round lower long lightPurple" @click="handleClick(0)" v-bind:class="{ 'lightPurple': selectedChoice[0].color === 'lightPurple', 'darkPurple': selectedChoice[0].color === 'darkPurple'}"> Want to help myself &nbsp;
   
-        <v-icon v-bind:class="{ 'dark': isLightPurple(0)}" center>
-          {{isLightPurple(0) ? 'add_circle_outline': 'remove_circle_outline'}}
+        <v-icon v-bind:class="{ 'dark': selectedChoice[0].color === 'lightPurple' }" center>
+          {{selectedChoice[0].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class="ml-6-restricted round lower x-long lightPurple" @click="handleClick(1)" v-bind:class="{ 'lightPurple': isLightPurple(1), 'darkPurple': isDarkPurple(1)}"> Want to help a loved one
-        <v-icon v-bind:class="{ 'dark': isLightPurple(1)}" center>
-          {{isLightPurple(1) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[1].color" class="ml-6-restricted round lower x-long lightPurple" @click="handleClick(1)" v-bind:class="{ 'lightPurple': selectedChoice[1].color === 'lightPurple' , 'darkPurple': selectedChoice[1].color === 'darkPurple'}"> Want to help a loved one &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[1].color === 'lightPurple' }" center>
+          {{selectedChoice[1].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class="ml-6-restricted-x round lower lightPurple" @click="handleClick(2)" v-bind:class="{ 'lightPurple': isLightPurple(2), 'darkPurple': isDarkPurple(2)}"> help others
-        <v-icon v-bind:class="{ 'dark': isLightPurple(2)}" center>
-          {{isLightPurple(2) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[2].color" class="ml-6-restricted-x round lower lightPurple" @click="handleClick(2)" v-bind:class="{ 'lightPurple': selectedChoice[2].color === 'lightPurple' , 'darkPurple': selectedChoice[2].color === 'darkPurple'}"> help others &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[2].color === 'lightPurple' }" center>
+          {{selectedChoice[2].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class=" round lower lightPurple ml-6-restricted" @click="handleClick(3)" v-bind:class="{ 'lightPurple': isLightPurple(3), 'darkPurple': isDarkPurple(3)}"> Am curious
-        <v-icon v-bind:class="{ 'dark': isLightPurple(3)}" center>
-          {{isLightPurple(3) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[3].color" class=" round lower lightPurple ml-6-restricted" @click="handleClick(3)" v-bind:class="{ 'lightPurple': selectedChoice[3].color === 'lightPurple' , 'darkPurple': selectedChoice[3].color === 'darkPurple'}"> Am curious &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[3].color === 'lightPurple' }" center>
+          {{selectedChoice[3].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
       <br class="visible-md-up">
       <br class="visible-md-up">
     </div>
   
-    <div class="row" v-if="findValues(0,4)">
+    <div class="row" v-if="containsValue(0,4)">
   
       <br class="visible-md-up">
-      <p class="col-md-8 ml-6 text-left lead"> I'd be willing to try </p>
+      <p class="col-md-8 ml-6 text-left lead"> I'd be willing to try  </p>
   
-      <v-btn v-model="selectedChoices" class="ml-6 round lower x-long lightPurple" @click="handleClick(4)" v-bind:class="{ 'lightPurple': isLightPurple(4), 'darkPurple': isDarkPurple(4)}"> Finger Tapping
-        <v-icon v-bind:class="{ 'dark': isLightPurple(4)}" center>
-          {{isLightPurple(4) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[4].color" class="ml-6 round lower x-long lightPurple" @click="handleClick(4)" v-bind:class="{ 'lightPurple': selectedChoice[4].color === 'lightPurple' , 'darkPurple': selectedChoice[4].color === 'darkPurple'}"> Finger Tapping &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[4].color === 'lightPurple' }" center>
+          {{selectedChoice[4].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class=" round lower lightPurple" @click="handleClick(5)" v-bind:class="{ 'lightPurple': isLightPurple(5), 'darkPurple': isDarkPurple(5)}"> Walking
-        <v-icon v-bind:class="{ 'dark': isLightPurple(5)}" center>
-          {{isLightPurple(5) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[5].color" class=" round lower lightPurple" @click="handleClick(5)" v-bind:class="{ 'lightPurple': selectedChoice[5].color === 'lightPurple' , 'darkPurple': selectedChoice[5].color === 'darkPurple'}"> Walking &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[5].color === 'lightPurple' }" center>
+          {{selectedChoice[5].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class="ml-6-restricted  round lower  lightPurple" @click="handleClick(6)" v-bind:class="{ 'lightPurple': isLightPurple(6), 'darkPurple': isDarkPurple(6)}"> Balance
-        <v-icon v-bind:class="{ 'dark': isLightPurple(6)}" center>
-          {{isLightPurple(6) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[6].color" class="ml-6-restricted  round lower  lightPurple" @click="handleClick(6)" v-bind:class="{ 'lightPurple': selectedChoice[6].color === 'lightPurple' , 'darkPurple': selectedChoice[6].color === 'darkPurple'}"> Balance &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[6].color === 'lightPurple' }" center>
+          {{selectedChoice[6].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
       <br class="visible-md-up">
       <br class="visible-md-up">
   
-      <p v-if="findValues(4,6)" class="col-md-8 ml-6 text-left lead"> I'd be willing to do this/these activities: </p>
-      <v-flex v-if="findValues(4,6)" class="col-sm-2 ml-6 col-4">
-        <v-radio label="Daily" value="radio-1" v-model="ex8"></v-radio>
+      <p v-if="containsValue(4,6)" class="col-md-8 ml-6 text-left lead"> I'd be willing to do this/these activities: </p>
+      <v-flex v-if="containsValue(4,6)" class="col-sm-2 ml-6 col-4">
+        <v-radio label="Daily" value="radio-1" v-model="radioChoice"></v-radio>
       </v-flex>
-      <v-flex v-if="findValues(4,6)" class="col-sm-2 col-8">
-        <v-radio value="3x per week" label="3x/week" v-model="ex8"></v-radio>
+      <v-flex v-if="containsValue(4,6)" class="col-sm-2 col-8">
+        <v-radio value="3x per week" label="3x/week" v-model="radioChoice"></v-radio>
       </v-flex>
-      <v-flex v-if="findValues(4,6)" class="col-sm-2 col-6">
-        <v-radio value="2x per week" label="2x/week" v-model="ex8"></v-radio>
+      <v-flex v-if="containsValue(4,6)" class="col-sm-2 col-6">
+        <v-radio value="2x per week" label="2x/week" v-model="radioChoice"></v-radio>
       </v-flex>
-      <v-flex v-if="findValues(4,6)" class="col-sm-2 col-6">
-        <v-radio value="weekly" label="weekly" v-model="ex8"></v-radio>
+      <v-flex v-if="containsValue(4,6)" class="col-sm-2 col-6">
+        <v-radio value="weekly" label="weekly" v-model="radioChoice"></v-radio>
       </v-flex>
   
     </div>
   
-    <!--<br class="visible-md-up" v-if="findValues(7,9)">-->
-    <div class="row" v-if="ex8 !== ''">
+    <!--<br class="visible-md-up" v-if="containsValue(7,9)">-->
+    <div class="row" v-if="radioChoice !== ''">
       <p class="col-md-8 ml-6 lead"> What would you like from us? </p>
-      <v-btn v-model="selectedChoices" class="ml-6 round lower x-long lightPurple" @click="handleClick(7)" v-bind:class="{ 'lightPurple': isLightPurple(7), 'darkPurple': isDarkPurple(7)}"> Updates on the study
-        <v-icon v-bind:class="{ 'dark': isLightPurple(7)}" center>
-          {{isLightPurple(7) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[7].color" class="ml-6 round lower x-long lightPurple" @click="handleClick(7)" v-bind:class="{ 'lightPurple': selectedChoice[7].color === 'lightPurple' , 'darkPurple': selectedChoice[7].color === 'darkPurple'}"> Updates on the study &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[7].color === 'lightPurple' }" center>
+          {{selectedChoice[7].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class="ml-6-restricted round lower lightPurple" @click="handleClick(8)" v-bind:class="{ 'lightPurple': isLightPurple(8), 'darkPurple': isDarkPurple(8)}"> Stats on participation
-        <v-icon v-bind:class="{ 'dark': isLightPurple(8)}" center>
-          {{isLightPurple(8) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[8].color" class="ml-6-restricted round lower lightPurple" @click="handleClick(8)" v-bind:class="{ 'lightPurple': selectedChoice[8].color === 'lightPurple' , 'darkPurple': selectedChoice[8].color === 'darkPurple'}"> Stats on participation &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[8].color === 'lightPurple' }" center>
+          {{selectedChoice[8].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-      <v-btn v-model="selectedChoices" class="round lower lightPurple ml-6-restricted " @click="handleClick(9)" v-bind:class="{ 'lightPurple': isLightPurple(9), 'darkPurple': isDarkPurple(9)}"> Personal performace
-        <v-icon v-bind:class="{ 'dark': isLightPurple(9)}" center>
-          {{isLightPurple(9) ? 'add_circle_outline': 'remove_circle_outline'}}
+      <v-btn v-model="selectedChoice[9].color" class="round lower lightPurple ml-6-restricted " @click="handleClick(9)" v-bind:class="{ 'lightPurple': selectedChoice[9].color === 'lightPurple' , 'darkPurple': selectedChoice[9].color === 'darkPurple'}"> Personal performace &nbsp;
+        <v-icon v-bind:class="{ 'dark': selectedChoice[9].color === 'lightPurple' }" center>
+          {{selectedChoice[9].color === 'lightPurple' ? 'fa-plus-circle': 'fa-times'}}
         </v-icon>
       </v-btn>
   
-    </div>
     <br>
-    <div class="row" v-if="findValues(7,9)">
-      <br class="visible-sm-up">
-      <br class="visible-sm-up">
+    <br>
+    </div>
+    <div class="row" v-if="containsValue(7,9)">
       <v-btn v-on:click="clicked" id="submit" class="col-md-2 ml-6" light> Submit</v-btn>
     </div>
+    <br>
+    <br>
     </div>
   </v-app>
 </template>
@@ -127,58 +127,24 @@ export default {
   name: 'subjectInterest',
   data () {
     return {
-      interestChoices: [
-        'want to help myself',
-        'want to help a loved one',
-        'help others',
-        'am curious'],
-      willingChoices: [
-        'finger tapping',
-        'walk',
-        'balance',
-        'finger to nose',
-        'short surveys'
-      ],
-      frequencyChoices: [
-        'daily',
-        '3x per week',
-        '2x per week',
-        'weekly'
-      ],
-      returnChoices: [
-        'updates on the study',
-        'stats on participation',
-        'personal performance'
-      ],
-      phoneChoices: [
-        'comfortable',
-        'weary',
-        'uncomfortable'
-      ],
-      ex8: '',
-      selectedChoices: ['g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g'],
-      lightPurpleDisk: 'g',
-      darkPurpleDisk: 'b',
-      turn: false
-    }
-  },
-  watch: {
-    selectedChoices: function () {
-      this.setIsInterested()
+      radioChoice: '',
+      selectedChoice: {
+        0: {color: 'lightPurple'},
+        1: {color: 'lightPurple'},
+        2: {color: 'lightPurple'},
+        3: {color: 'lightPurple'},
+        4: {color: 'lightPurple'},
+        5: {color: 'lightPurple'},
+        6: {color: 'lightPurple'},
+        7: {color: 'lightPurple'},
+        8: {color: 'lightPurple'},
+        9: {color: 'lightPurple'}
+      }
     }
   },
   methods: {
     clicked () {
       this.$router.push('Eligibility')
-    },
-    findValues: function (start, stop) {
-      var inUse = 0
-      for (var i = start; i < stop; i++) {
-        if (this.selectedChoices[i] === 'b') {
-          inUse++
-        }
-      }
-      return (inUse > 0)
     },
     scrollPage: _.debounce(
       function (arg1) {
@@ -186,22 +152,19 @@ export default {
       }
     , 200),
     handleClick: function (index) {
-      this.turn = !this.turn
-      this.turn ? this.selectedChoices[index] = this.darkPurpleDisk : this.selectedChoices[index] = this.lightPurpleDisk
-    },
-    isLightPurple: function (index) {
-      return (this.selectedChoices[index] === this.lightPurpleDisk)
-    },
-    isDarkPurple: function (idx1) {
-      return !(this.turn == null) && (this.selectedChoices[idx1] === this.darkPurpleDisk)
-    },
-    isInitial: function (index) {
-      if (this.intial[index]) {
-        this.intial[index] = false
-        return true
+      if (this.selectedChoice[index].color === 'darkPurple') {
+        this.selectedChoice[index].color = 'lightPurple'
       } else {
-        return this.intial[index]
+        this.selectedChoice[index].color = 'darkPurple'
       }
+    },
+    containsValue: function (start, stop) {
+      for (var i = start; i < stop; i++) {
+        if (this.selectedChoice[i].color === 'darkPurple') {
+          return true
+        }
+      }
+      return false
     }
   },
   directives: {
