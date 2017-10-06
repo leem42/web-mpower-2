afb89e6f672c966393525b9a3fb80232e223f708
michael lee <keymikey54@yahoo.com>
2017-10-06 15:54:33 -0700

    added mobile registration shell

---
+++ src/components/on_boarding_components/registration/RegistrationMobile.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+        <p class="lead font-weight-bold mx-auto col-md-8"> 
+            Enter your mobile phone number to recieve your download link for the app 
+        </p>
+    </div>
+
+    </v-app>
+</template>
